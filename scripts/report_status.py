f95f32259ab91c32d1b7356cc6dc8cb881dee875
krvzdrv <111345161+krvzdrv@users.noreply.github.com>
2025-06-25 17:06:20 +0300

    Заголовок отчета по статусам теперь в формате WORONKA_DD.MM.YYYY, после заголовка два переноса строки, как в отчете по выручке

--- scripts/report_status.py
+++ scripts/report_status.py
@@ -296,8 +296,8 @@ def main():
         
         # Отправляем один общий отчет
         if all_reports:
-            final_report_header = f"WORONKA_{today.strftime('%d_%m_%Y')}"
-            final_report = f"```{final_report_header}\n" + "\n\n".join(all_reports) + "\n```"
+            final_report_header = f"WORONKA_{today.strftime('%d.%m.%Y')}"
+            final_report = f"```{final_report_header}\n\n" + "\n\n".join(all_reports) + "\n```"
             send_to_telegram(final_report)
 
     except psycopg2.Error as e:
